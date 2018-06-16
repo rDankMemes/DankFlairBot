32d6cbc14576b38cb6b14421d1745430c8a12264
ELFAHBEHT_SOOP <admin@elfahbeht.xyz>
2018-06-15 19:37:12 -0500

    Updating how information is displayed.

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import time
 import ThreadedServices
 from DatabaseManager import DatabaseManager
 from RedditManagerUtils import RedditManager
+from DisplayManager import DisplayManager
 from ruamel.yaml import YAML
 import threading
 import Rule
 from RulesManager import RulesManager
+import sys
 
 SUBREDDIT = "dankmemes"
 
@@ -21,7 +23,28 @@ def main():
 
     RedditManager.login_threads_from_file("config.yml")
 
+    ThreadedServices.read_setting_from_file("settings.yml")
+
+#    DisplayManager.addSubreddit("BotParty")
+
+
+ #   for i in range(100):
+  #      time.sleep(0.1)
+   #     DisplayManager.update()
+
+#     RedditManager.send_message("This is a test",
+# """Wow!
+#
+# This is going ***MULTILINE***
+#
+# I hope it works.
+#
+# """, recipient="ELFAHBEHT_SOOP")
+
+#    ThreadedServices.setup_threads("BotParty")
+
     ThreadedServices.setup_threads("CongratsLikeImFive")
+
 
     #    RedditManager.get_bans("BotParty")
 
